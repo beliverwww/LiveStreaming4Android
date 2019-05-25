dbc2b938d1ad9185684fe6c270ee2ea8662814e6
beliverwww <beliverwww@protonmail.com>
2019-05-25 12:36:04 +0300

    Adding SplashActivity class and made splash gui.

---
+++ app/src/main/java/com/livestream/rtmpsend/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.livestream.rtmpsend;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SplashActivity extends Activity {
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
